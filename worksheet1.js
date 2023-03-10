7fd7549ebbb2f0d71ef18d1bd014b21df0244b6e
Parksiryu <122608754+Parksiryu@users.noreply.github.com>
2023-03-10 09:58:39 +0900

    Add files via upload

---
+++ worksheet1.js
@@ -0,0 +1,390 @@
+let problem = Math.floor(Math.random() * 5);
+let quizData = [
+    {
+        question: "is a segment that connects the midpoints of two sides.",
+        a: "Median",
+        b: "Angle Bisector",
+        c: "Midsegment",
+        d: "Altitude",
+        e: "Perpendicular bisector",
+        correct: "c",
+    },
+    {
+        question: "is a segment that bisects an angle.",
+        a: "Perpendicular bisector",
+        b: "Midsegment",
+        c: "Altitude",
+        d: "Angle bisector",
+        e: "Median",
+        correct: "d",
+    },
+    {
+        question: "is a segment that bisects a side and is perpendicular to that side.",
+        a: "Altitude",
+        b: "Angle Bisector",
+        c: "Perpendicular bisector",
+        d: "Median",
+        e: "Midsegment",
+        correct: "c",
+    },
+    {
+        question: "is a segment that connects a vertex to the midpoints of the opposite side.",
+        a: "Midsegment",
+        b: "Median",
+        c: "Altitude",
+        d: "Perpendicular Bisector",
+        e: "Angle bisector",
+        correct: "b",
+    },
+    {
+        question: "is a segment from a vertex, perpendicular to the opposite side.",
+        a: "Altitude",
+        b: "Perpendicular Bisector",
+        c: "Midsegment",
+        d: "Median",
+        e: "Angle bisector",
+        correct: "a",
+    },
+
+]
+
+if (problem == 1){
+    quizData = [
+        {
+            question: "is a segment that connects the midpoints of two sides.",
+            a: "Angle Bisector",
+            b: "Median",
+            c: "Perpendicular Bisector",
+            d: "Midsegment",
+            e: "Altitude",
+            correct: "d",
+        },
+        {
+            question: "is a segment that bisects an angle.",
+            a: "Midsegment",
+            b: "Perpendicular Bisector",
+            c: "Median",
+            d: "Altitude",
+            e: "Angle Bisector",
+            correct: "e",
+        },
+        {
+            question: "is a segment that bisects a side and is perpendicular to that side.",
+            a: "Angle Bisector",
+            b: "Altitude",
+            c: "Midsegment",
+            d: "Perpendicular Bisector",
+            e: "Meidan",
+            correct: "d",
+        },
+        {
+            question: "is a segment that connects a vertex to the midpoints of the opposite side.",
+            a: "Median",
+            b: "Midsegment",
+            c: "Angle Bisector",
+            d: "Altitude",
+            e: "Perpendicular Bisector",
+            correct: "a",
+        },
+        {
+            question: "is a segment from a vertex, perpendicular to the opposite side.",
+            a: "Perpendicular Bisector",
+            b: "Altitude",
+            c: "Angle Bisector",
+            d: "Midsegment",
+            e: "Median",
+            correct: "b",
+        },
+    
+    ];
+}
+else if (problem == 2){
+    quizData = [
+        {
+            question: "is a segment that connects the midpoints of two sides.",
+            a: "Angle Bisector",
+            b: "Midsegment",
+            c: "Median",
+            d: "Perpendicular",
+            e: "Altitude",
+            correct: "b",
+        },
+        {
+            question: "is a segment that bisects an angle.",
+            a: "Midsegment",
+            b: "Altitude",
+            c: "Perpendicular bisector",
+            d: "Median",
+            e: "Angle Bisector",
+            correct: "e",
+        },
+        {
+            question: "is a segment that bisects a side and is perpendicular to that side.",
+            a: "Angle Bisector",
+            b: "Perpendicular Bisector",
+            c: "Altitude",
+            d: "Midsegment",
+            e: "Median",
+            correct: "b",
+        },
+        {
+            question: "is a segment that connects a vertex to the midpoints of the opposite side.",
+            a: "Median",
+            b: "Altitude",
+            c: "Midsegment",
+            d: "Angle bisector",
+            e: "Perpendicular Bisector",
+            correct: "a",
+        },
+        {
+            question: "is a segment from a vertex, perpendicular to the opposite side.",
+            a: "Altitude",
+            b: "Perpendicular Bisector",
+            c: "Midsegment",
+            d: "Median",
+            e: "Angle bisector",
+            correct: "a",
+        },
+    
+    ];
+}
+
+else if (problem == 3){
+    quizData = [
+        {
+            question: "is a segment that connects the midpoints of two sides.",
+            a: "Midsegment",
+            b: "Altitude",
+            c: "Median",
+            d: "Angle Bisector",
+            e: "Perpendicular bisector",
+            correct: "a",
+        },
+        {
+            question: "is a segment that bisects an angle.",
+            a: "Altitude",
+            b: "Angle Bisector",
+            c: "Perpendicular Bisector", 
+            d: "Midsegment",
+            e: "Median",
+            correct: "b",
+        },
+        {
+            question: "is a segment that bisects a side and is perpendicular to that side.",
+            a: "Perpendicular bisector",
+            b: "Median",
+            c: "Altitude",
+            d: "Angle Bisector",
+            e: "Midsegment",
+            correct: "a",
+        },
+        {
+            question: "is a segment that connects a vertex to the midpoints of the opposite side.",
+            a: "Altitude",
+            b: "Perpendicular Bisector",
+            c: "Midsegment",
+            d: "Median",
+            e: "Angle bisector",
+            correct: "d",
+        },
+        {
+            question: "is a segment from a vertex, perpendicular to the opposite side.",
+            a: "Midsegment",
+            b: "Median",
+            c: "Altitude",
+            d: "Perpendicular Bisector",
+            e: "Angle bisector",
+            correct: "c",
+        },
+    
+    ];
+}
+
+else if (problem == 4){
+    quizData = [
+        {
+            question: "is a segment that connects the midpoints of two sides.",
+            a: "Median",
+            b: "Altitude",
+            c: "Perpendicular Bisector",
+            d: "Angle Bisector",
+            e: "Midsegment",
+            correct: "e",
+        },
+        {
+            question: "is a segment that bisects an angle.",
+            a: "Perpendicular bisector",
+            b: "Angle Bisector",
+            c: "Median",
+            d: "Midsegment",
+            e: "Altitude",
+            correct: "b",
+        },
+        {
+            question: "is a segment that bisects a side and is perpendicular to that side.",
+            a: "Altitude",
+            b: "Median",
+            c: "Midsegment",
+            d: "Angle Bisector",
+            e: "Perpendicular Bisector",
+            correct: "e",
+        },
+        {
+            question: "is a segment that connects a vertex to the midpoints of the opposite side.",
+            a: "Midsegment",
+            b: "Median",
+            c: "Altitude",
+            d: "Perpendicular Bisector",
+            e: "Angle bisector",
+            correct: "b",
+        },
+        {
+            question: "is a segment from a vertex, perpendicular to the opposite side.",
+            a: "Altitude",
+            b: "Median",
+            c: "Angle Bisector",
+            d: "Perpendicular Bisector",
+            e: "Midsegment",
+            correct: "a",
+        },
+    
+    ];
+}
+
+else if (problem == 5){
+    quizData = [
+        {
+            question: "is a segment that connects the midpoints of two sides.",
+            a: "Perpendicular Bisector",
+            b: "Altitude",
+            c: "Median",
+            d: "Angle Bisector",
+            e: "Midsegment",
+            correct: "e",
+        },
+        {
+            question: "is a segment that bisects an angle.",
+            a: "Median",
+            b: "Angle bisector",
+            c: "Perpendicular bisector",
+            d: "Midsegment",
+            e: "Altitude",
+            correct: "b",
+        },
+        {
+            question: "is a segment that bisects a side and is perpendicular to that side.",
+            a: "Midsegment",
+            b: "Median",
+            c: "Altitude",
+            d: "Angle Bisector",
+            e: "Perpendicular bisector",
+            correct: "e",
+        },
+        {
+            question: "is a segment that connects a vertex to the midpoints of the opposite side.",
+            a: "Angle bisector",
+            b: "Perpendicular Bisector",
+            c: "Midsegment",
+            d: "Median",
+            e: "Altitude",
+            correct: "d",
+        },
+        {
+            question: "is a segment from a vertex, perpendicular to the opposite side.",
+            a: "Angle Bisector",
+            b: "Median",
+            c: "Altitude",
+            d: "Perpendicular Bisector",
+            e: "Midsegment",
+            correct: "c",
+        },
+    
+    ];
+}
+
+
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+
+    //While there remain elements to shuffle…
+    while (currentIndex != 0) {
+        //Pick a remaining element…
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        //And swap it with the current element. 
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+    return array;
+}
+
+const quizeData = shuffle(quizData)
+
+
+const quiz= document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const e_text = document.getElementById('e_text')
+const submitBtn = document.getElementById('submit')
+
+
+let currentQuiz = 0
+let score = 0
+
+
+
+loadQuiz()
+
+function loadQuiz() {
+
+    deselectAnswers()
+
+    const currentQuizData = quizeData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+    e_text.innerText = currentQuizData.e
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+       if(answer === quizData[currentQuiz].correct) {
+           score++
+       }
+
+       currentQuiz++
+
+       if(currentQuiz < quizData.length) {
+           loadQuiz()
+       } else {
+           quiz.innerHTML = `
+           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+
+           <button onclick="location.reload()">Reload</button>
+           `
+       }
+    }
+})
